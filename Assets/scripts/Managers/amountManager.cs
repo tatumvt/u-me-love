67735d8c920d7271da4f07d3454b270b1e3c5fd3
Tatum van Trier <t.van.trier@flavour.nl>
2023-02-27 12:02:57 +0100

    adds answer checking lvl2

--- Assets/scripts/Managers/amountManager.cs
+++ Assets/scripts/Managers/amountManager.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class amountManager : MonoBehaviour
 {
     //0 = flour || 1 = choco || 2 = sugar || 3 = milk || 4 = egg 
     public GameObject[] amounts = new GameObject[5];
     //
-    private List<int> theScores;
+    public List<int> theScores;
 
     public void Init()
     {
@@ -29,5 +30,31 @@ public class amountManager : MonoBehaviour
         {
             amounts[i].GetComponent<TextMeshProUGUI>().text = "" + theScores[i];
         }
+    }
+    public void answerCheck()
+    {
+        if (theScores != null)
+        {
+            if (theScores[0] == 11 && theScores[1] == 5 && theScores[2] == 6 && theScores[3] == 3 && theScores[4] == 0)
+            {
+                LoadSceneScript("lvl02RIGHT");
+            }
+            else
+            {
+                LoadSceneScript("lvl02WRONG");
+            }
+        }
+    }
+    public void LoadSceneScript(string name)
+    {
+        Time.timeScale = 1;
+        //bc.buttonClick();
+        StartCoroutine(LoadSceneScriptIE(name));
+    }
+    private IEnumerator LoadSceneScriptIE(string name)
+    {
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(0.3f);
+        SceneManager.LoadScene(name);
     }
 }
