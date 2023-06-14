09b1d2785622913a19b92acd4bc7093fa7c89a79
Tatum van Trier <t.van.trier@flavour.nl>
2023-06-14 17:11:14 +0200

    adds answer block timers

---
+++ Assets/scripts/answerBlockTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class answerBlockTimer : MonoBehaviour
+{
+    public GameObject btn;
+
+    // Update is called once per frame
+    void Start()
+    {
+        WaitForAnswer();
+    }
+    public void WaitForAnswer()
+    {
+        Time.timeScale = 1;
+        StartCoroutine(IEWaitForAnswer());
+    }
+    private IEnumerator IEWaitForAnswer()
+    {
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(12f);
+         btn.SetActive(true);
+    }
+}
